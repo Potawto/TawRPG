a0d821fb842b6fff48c9674103581aac53348d48
Taw <eyvindm@gmail.com>
2024-06-04 14:56:34 +0200

    location border

--- Scripts/Location.gd
+++ Scripts/Location.gd
@@ -1,21 +1,33 @@
 extends Node
 
-@export var size := 16
+@export var size: int = 16
+@export var main_terrain_type: int = 0
+@export var border_terrain_type: int = 1
 
-var _cells := []
+var _cells_main := []
+var _cells_border := []
 
 @onready var tile_map := $TileMap as TileMap
 @onready var player := $Player as CharacterBody2D
 
 func _ready() -> void:
-	_cells.resize(size * size)
-	var k = 0
-	for i in range(size):
-		for j in range(size):
-			#tile_map.set_cell(0, Vector2i(i, j), 0, Vector2i(0, 0), 0)
-			_cells[k] = Vector2i(i, j)
-			k += 1
-	tile_map.set_cells_terrain_connect(0, _cells, 0, 0, false)
+	_cells_main.resize(size * size)
+	_cells_border.resize(4 * (size + 1))
+	var k = 0 #number of main cells
+	var l = 0 #number of border cells
+	for i in range(-1, size+1):
+		for j in range(-1, size+1):
+			var cell = Vector2i(i, j)
+			if i == -1 or i == size or j == -1 or j == size:
+				_cells_border[l] = cell
+				l += 1
+			else:
+				_cells_main[k] = cell
+				k += 1
+				
+	tile_map.set_cells_terrain_connect(0, _cells_main, 0, main_terrain_type, false)
+	tile_map.set_cells_terrain_connect(0, _cells_border, 0, border_terrain_type, false)
+	
 	
 	var tilemap := $TileMap as TileMap
 	var tilesize = tilemap.tile_set.tile_size
